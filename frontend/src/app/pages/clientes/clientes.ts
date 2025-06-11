f44a2cbfa62ac94a876dcb7cbb769876b3b3f367
kikeProgrammer007 <xthewonderlanx25996@gmail.com>
2025-06-10 23:08:35 -0400

    Actualiza el diseño del layout principal, corrige la ruta de la imagen del usuario en el sidebar y agrega la página de 'Clientes' con su respectivo componente y estructura de contenido.

---
+++ frontend/src/app/pages/clientes/clientes.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-clientes',
+  imports: [CommonModule],
+  templateUrl: './clientes.html'
+})
+export class Clientes {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  crearcliente(){
+    
+  }
+}
